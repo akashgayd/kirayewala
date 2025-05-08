44e5fdfe952ae4931132af805e60a305671ca374
unknown <akashgaydhane781@gmail.com>
2025-05-08 12:52:41 +0530

    fff

--- models/User.js
+++ models/User.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   otp: { type: String },
   otpExpires: { type: Date },
-  role: { type: String, enum: ['user', 'provider']},
+  role: { type: String, enum: ['user', 'provider'], default: 'provider' },
   favorites: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Property' }],
 }, { timestamps: true });
 
